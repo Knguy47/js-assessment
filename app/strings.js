28a8a787d0e27507b548340efa60d84b5cf31958
Kevin Nguyen <knguy47@gmail.com>
2017-06-30 15:06:01 -0500

    Complete string function string reduce

--- app/strings.js
+++ app/strings.js
@@ -2,18 +2,29 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.stringsAnswers = {
   reduceString: function(str, amount) {
-    let stringArray = str.split('');
-    let storage = {};
-    stringArray.reduce((acc, letter) => {
-    
-    })
+    let counter = 0;
+    let result = '';
+
+    for(let i = 0; i < str.length; i++) {
+      if(str[i] !== str[i - 1]) {
+        counter = 0;
+      }
+
+      if(counter < amount) {
+        result += str[i]
+        counter++;
+      }
+    }
+
+    return result;
   },
 
   wordWrap: function(str, cols) {
+    let words = str.split(' ');
     let result = '';
     
-    for(var i = 1; i < str.length; i++) {
-      result += str.slice(i, i+cols+1) + '\n';
+    for(var i = 1; i < words.length; i++) {
+      result += words[i] + '\n';
     }
     
     return result;
