7d427b2a3bd7c1cd6b07ca7973dff9a3cc3b9edc
Kevin Nguyen <knguy47@gmail.com>
2017-07-02 23:55:22 -0500

    Finish number modules

--- app/numbers.js
+++ app/numbers.js
@@ -2,19 +2,47 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.numbersAnswers = {
   valueAtBit: function(num, bit) {
-    
+    const binary = this.convertToBinary(num);
+    return parseInt(binary[binary.length - bit], 10);
   },
 
   base10: function(str) {
     return parseInt(str, 2);
   },
 
-  convertToBinary: function(num) {
-    let hex = (num).to
-    return this.base10(hex)
+  convertToBinary: function convertToBinary(num) {
+    let binary = num.toString(2);
+    const numZeros = 8 - binary.length;
+    for (let i = 0; i < numZeros; i++) {
+      binary = `0${binary}`;
+    }
+
+    return binary;
   },
 
   multiply: function(a, b) {
-    return (a * b).toFixed();
+    // I was unable to solve this without looking at the answer, 
+    // but I spent time to understand the solution.
+    
+    a = adjust(a);
+    b = adjust(b);
+
+    function adjust(num) {
+      let exponent, multiplier;
+
+      if (num < 1) {
+        exponent = Math.floor( Math.log(num) * -1 );
+        multiplier = Math.pow(10, exponent);
+
+        return {adjusted: num * multiplier, multiplier: multiplier};
+      }
+
+      return {adjusted: num, multiplier: 1};
+    }
+
+    let result = (a.adjusted * b.adjusted) / (a.multiplier * b.multiplier);
+
+    return result;
+
   }
 };
