26e24a6b98a9d9f2917b00a6cffccc0ce945b2f9
Kevin Nguyen <knguy47@gmail.com>
2017-07-02 23:52:54 -0500

    Fix style guides for ES6

--- app/functions.js
+++ app/functions.js
@@ -10,14 +10,14 @@ exports.functionsAnswers = {
   },
 
   functionFunction: function(str) {
-    let oldStr = str;
+    const oldStr = str;
     return function(str){return oldStr + ', ' + str};
   },
 
   makeClosures: function(arr, fn) {
-    let result = function(arr, fn) {
+    const result = function(arr, fn) {
       return arr.map((element) => {  
-        let closure = function() {
+        const closure = function() {
           return fn(element);
         };
 
@@ -29,25 +29,25 @@ exports.functionsAnswers = {
   },
 
   partial: function(fn, str1, str2) {
-    let partialFn = fn.bind(this, str1, str2);
+    const partialFn = fn.bind(this, str1, str2);
     return partialFn;
   },
 
   useArguments: function() {
-    let args = Array.prototype.slice.call(arguments);
+    const args = Array.prototype.slice.call(arguments);
     return args.reduce((acc, element) => {
       return acc + element;
     });
   },
 
   callIt: function(fn) {
-    let args = Array.prototype.slice.call(arguments, 1);
+    const args = Array.prototype.slice.call(arguments, 1);
     return fn.apply(this, args);
   },
 
   partialUsingArguments: function(fn) {
-    let args = Array.prototype.slice.call(arguments, 1);
-    let partialFn = fn.bind(this, ...args);
+    const args = Array.prototype.slice.call(arguments, 1);
+    const partialFn = fn.bind(this, ...args);
     
     return partialFn;
   },
