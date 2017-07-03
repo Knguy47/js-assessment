1a2a0f106efdc47a4d5bb6ac444ceb563502441f
Kevin Nguyen <knguy47@gmail.com>
2017-07-02 23:51:13 -0500

    Fix style guides for ES6

--- app/bestPractices.js
+++ app/bestPractices.js
@@ -9,7 +9,7 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.bestPracticesAnswers = {
   globals: function() {
-    let myObject = {
+    const myObject = {
       name: 'Jory'
     };
 
