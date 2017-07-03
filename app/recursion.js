8c5ed197425e6353bec8c8fd268f09887ce83ff8
Kevin Nguyen <knguy47@gmail.com>
2017-07-02 23:57:40 -0500

    Finish validParentheses function

--- app/recursion.js
+++ app/recursion.js
@@ -2,22 +2,22 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.recursionAnswers = {
   listFiles: function(data, dirName) {
-  let result = [];
+  const result = [];
     
     function searchDir(dir, subDirectory) {
-      let targetDirectory = subDirectory || dir.dir;
-      let directory = dir.dir;
-      let files = dir.files || [];
+      const targetDirectory = subDirectory || dir.dir;
+      const directory = dir.dir;
+      const files = dir.files || [];
       
-      for(let i = 0; i < files.length; i++) {
-        if(targetDirectory === directory) {
-          if(typeof files[i] === 'string') {
+      for (let i = 0; i < files.length; i++) {
+        if (targetDirectory === directory) {
+          if (typeof files[i] === 'string') {
            result.push(files[i]);
           } else {
             searchDir(files[i], files[i].dir);
           }
         } else {
-          if(files[i].dir) {
+          if (files[i].dir) {
             searchDir(files[i], subDirectory);
           }
         }
@@ -28,16 +28,16 @@ exports.recursionAnswers = {
   },
 
   permute: function(arr) {
-    let permutations = [];
-    let stack = [];
+    const permutations = [];
+    const stack = [];
 
     function permutate() {
-      if(arr.length === 0) {
+      if (arr.length === 0) {
         permutations.push(stack.slice());
       }
 
-      for(var i = 0; i < arr.length; i++) {
-        let element = arr.splice(i, 1);
+      for (var i = 0; i < arr.length; i++) {
+        const element = arr.splice(i, 1);
         stack.push(element);
         permutate();
         stack.pop();
@@ -46,7 +46,8 @@ exports.recursionAnswers = {
     }
 
     permutate();
-    for (var i = 0; i < permutations.length; i++) {
+
+    for (let i = 0; i < permutations.length; i++) {
       permutations[i] = permutations[i].join('').split('');
     }
  
@@ -55,7 +56,7 @@ exports.recursionAnswers = {
 
   fibonacci: function(n) {
     function fib(n) {
-      if(n === 1) {
+      if (n === 1) {
         return 1;
       } else if (n === 0) {
         return 0;
@@ -68,24 +69,24 @@ exports.recursionAnswers = {
   },
 
   validParentheses: function(n) {
-    let balancedParens = function(input) {
-      let key = {
+    const balancedParens = function(input) {
+      const key = {
         "(": ")",
         "[": "]",
         "{": "}"
       };
 
-      let reversekey = {
+      const reversekey = {
         ")": true,
         "]": true,
         "}": true
       };
 
-      let array = input.split('');
-      let store = [];
+      const array = input.split('');
+      const store = [];
 
       for (let i = 0; i < array.length; i++) {
-        let char = array[i];
+        const char = array[i];
         
         if(key[char]) {
           store.push(char);
@@ -99,18 +100,18 @@ exports.recursionAnswers = {
       return store.length === 0;
     };
 
-    let openParens = new Array(n);
-    let closeParens = new Array(n);
-    openParens.fill('(');
-    closeParens.fill(')');
-    let parens = openParens.concat(closeParens);
-    let result = this.permute(parens);
-    // console.log(result)
-    let filteredResult = result.filter((element) => {
-      // console.log(balancedParens(element.join('')))
+    const openParens = new Array(n).fill('(');
+    const closeParens = new Array(n).fill(')');
+    
+    const parens = [...openParens, ...closeParens];
+    const result = this.permute(parens);
+    
+    const filteredResult = result.filter((element) => {
       return balancedParens(element.join(''));
+    }).map((element) => {
+      return element.join('');
     });
-    // console.log(filteredResult)
-    return filteredResult;
+
+    return [...new Set(filteredResult)];
   }
 };
